05edcf04a79c341fc56f4a50f2b08ba927af8f51
AJ <richie.okoro@gmail.com>
2025-07-27 11:10:29 +0100

    navbar started

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import "../app.css"
+    import Nav from "./Nav.svelte";
     let { children } = $props();
 </script>
 
+<header><Nav/></header>
 <main>{@render children()}</main>
---
+++ frontend/src/routes/Nav.svelte
@@ -0,0 +1,40 @@
+<nav>
+    <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+    <ul class="auth-links">
+        <li><a href="/#">Login</a></li>
+        <li><a href="/#">Signup</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        background-color: #333;
+        color: white;
+        padding: 1rem;
+    }
+    ul {
+        display: flex;
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        /* display: inline; */
+        margin-right: 1rem;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    .auth-links {
+        align-self: flex-end;
+    }
+</style>
