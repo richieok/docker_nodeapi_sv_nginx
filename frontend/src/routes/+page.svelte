06fde1716d9a64059f18f5cf7e1ef52e0654776d
AJ <richie.okoro@gmail.com>
2025-07-27 12:11:03 +0100

    homepage centered

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>DevConZero Server powered by SvelteKit</h1>
+<div>
+    <h1>DevConZero Server powered by SvelteKit</h1>
+</div>
+
+<style>
+    div {
+        display: grid;
+        place-items: center;
+        height: calc(100vh - 60px); /* Adjust based on header height */
+    }
+</style>
